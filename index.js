cb46f8e2d887fcf309c8594bf8d7c66d2efaa120
maning711 <277943578@qq.com>
2016-01-03 21:15:11 +0800

    add the contents and server action
    Signed-off-by: maning711 <277943578@qq.com>

--- index.js
+++ index.js
@@ -4,11 +4,16 @@ var express = require('express')
   , cookieParser = require('cookie-parser')
   , session = require('express-session');
 
+/**
+ * create the app
+ */
 app = express();
 
+/**
+ * set the middleware
+ */
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
-
 app.use(cookieParser());
 app.use(session({
 	resave: false,
@@ -16,20 +21,39 @@ app.use(session({
 	secret : 'my secret'
 }));
 
+/**
+ * set view item
+ */
 app.set('view engine', 'jade');
 
-app.set('view option', { layout : false });
-
+/**
+ * default route
+ */
 app.get('/', function (req, res) {
 	res.render('index', { authenticated : false });
 });
 
+/**
+ * login route
+ */
 app.get('/login', function (req, res) {
 	res.render('login');
 });
 
+/**
+ * signup route
+ */
 app.get('/signup', function (req, res) {
 	res.render('signup');
 });
 
+/**
+ * connect to db
+ */
+var server = new mongodb.Server('123.0.0.1', 27017);
+
+
+/**
+ * listener
+ */
 app.listen(3000);
