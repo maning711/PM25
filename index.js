419d7aabc6aff9d7ef7fb7fe76f33fde0227179b
maning711 <277943578@qq.com>
2016-01-08 18:37:42 +0900

    complete the login and signup, to get the pm2.5 data is prepared Signed-off-by: unknown <maning1@WIN7-AHT8CL1I83.trans-cosmos.com.cn>

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ var express = require('express')
   , mongodb = require('mongodb')
   , bodyParser = require('body-parser')
   , cookieParser = require('cookie-parser')
-  , session = require('express-session');
+  , session = require('express-session')
+  , superagent = require('superagent');
 
 /**
  * create the app
@@ -20,9 +21,27 @@ app.use(bodyParser.json())
 app.use(cookieParser());
 app.use(session({
 	resave: false,
-    saveUninitialized: true,
+  saveUninitialized: true,
 	secret : 'my secret'
 }));
+
+/**
+ * authenticated check
+ */
+app.use(function (req, res, next) {
+  if (req.session.loggedIn) {
+    var objectID = mongodb.ObjectID;
+    res.locals.authenticated = true;
+    app.users.findOne({ _id: objectID(req.session.loggedIn) }, function (err, doc) {
+      if (err) return next(err);
+      res.locals.me = doc;
+      next();
+    });
+  } else {
+    res.locals.authenticated = false;
+    next();
+  }
+});
 
 /**
  * set view item
@@ -33,14 +52,34 @@ app.set('view engine', 'jade');
  * default route
  */
 app.get('/', function (req, res) {
-	res.render('index', { authenticated : false });
+	res.render('index');
 });
 
 /**
  * login route
  */
+ app.get('/login', function (req, res) {
+  res.render('login');
+});
+
+ /**
+ * logined route
+ */
 app.get('/login/:signupEmail', function (req, res) {
 	res.render('login', { signupEmail : req.params.signupEmail });
+});
+
+/**
+ * login post route
+ */
+app.post('/login', function (req, res) {
+  app.users.findOne({ email : req.body.user.email, password : req.body.user.password },
+    function (err, doc) {
+      if (err) return next(err);
+      if (!doc) return res.send('<p>User not found. Go back and try again.</p>');
+      req.session.loggedIn = doc._id.toString();
+      res.redirect('/');
+    });
 });
 
 /**
@@ -61,15 +100,39 @@ app.post('/signup', function (req, res, next) {
 });
 
 /**
+ * logout route
+ */
+app.get('/logout', function (req, res) {
+  req.session.loggedIn = null;
+  res.redirect('/');
+});
+
+/**
  * connect to db
  */
 var MongoClient = mongodb.MongoClient;
 var DB_CONN_STR = 'mongodb://localhost:27017/test';
 
 MongoClient.connect(DB_CONN_STR, function(err, db) {
+  if (err) throw err;
+  app.users = db.collection('maning');
+  console.log('\033[96m + \033[39m connected to mongodb');
+
+  db.ensureIndex('users', 'email', function (err) {
     if (err) throw err;
-    console.log('\033[96m + \033[39m connected to mongodb');
-    app.users = db.collection('maning');
+    db.ensureIndex('users', 'email', function (err) {
+      if (err) throw err;
+      console.log('\033[96m + \033[39m ensered indexes');
+    });
+  });
+
+  superagent.post('http://api.lib360.net/open/pm2.5.json?city=天津')
+  .set('Accept', 'application/json')
+  .end(function (err, res) {
+    if (!err) {
+      console.log(res.text);
+    }
+  });
 });
 
 /**
